23a72810691454f5dd9d35b03b326d87749f9cfc
Ratanont Sukapan <ratanont.s@gmail.com>
2024-10-29 19:55:45 +0700

    Update colors and font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const MENUS_LEFT = [
+  { name: "Men", route: "/products" },
+  { name: "Women", route: "/products" },
+  { name: "Kids", route: "/products" },
+  { name: "Shoes", route: "/products" },
+  { name: "Accessories", route: "/products" },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-secondary text-white py-2">
+      <div className="container">
+        <div className="flex items-center">
+          <Link to="/">Logo</Link>
+          <ul className="flex gap-4">
+            {MENUS_LEFT?.map((menu) => (
+              <li key={menu.name}>
+                <Link to={menu.route}>{menu.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        primary: {
+          700: "#C1CD00",
+          DEFAULT: "#DEF81C",
+          400: "#DFF547",
+          300: "#E5F96E",
+          100: "#F6FCC3",
+          50: "#FBFFE7",
+        },
+        secondary: {
+          DEFAULT: "#222222",
+          700: "#626262",
+          500: "#9F9F9F",
+          300: "#E1E1E1",
+          100: "#F5F5F5",
+          50: "#FAFAFA",
+        },
+        success: "#13CE66",
+        info: "#3366FF",
+        warning: "#FFB020",
+        danger: "#FF000D",
+      },
+    },
   },
   plugins: [],
 };
