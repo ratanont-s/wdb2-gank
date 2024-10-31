aaffef769810c8c398cf6ae6b4f7f1d61dce132c
Pitak Mennakret <pitakpxt@gmail.com>
2024-10-31 22:13:41 +0700

    feature home initialized

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-// import heroImage from "./public/hero-image.png";
+import heroImage from "../../public/hero-image.png";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <img src={heroImage} alt="hero-image" />
+    <div className="relative w-screen -mx-[50vw] left-[50%] right-[50%] h-[166px] md:h-[420px]">
+      <img
+        src={heroImage}
+        alt="hero-image"
+        className="w-full h-full object-cover"
+      />
     </div>
   );
 }
