0da26021b8db70c0d3db06645b6c1531179815bd
Ratanont Sukapan (Nont) <ratanont.s@gmail.com>
2024-11-01 19:57:03 +0700

    fix: path image hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import heroImage from "../../public/hero-image.png";
 
 export default function Hero() {
   return (
     <div className="relative w-screen -mx-[50vw] left-[50%] right-[50%] h-[166px] md:h-[420px]">
       <img
-        src={heroImage}
+        src='/hero-image.png'
         alt="hero-image"
         className="w-full h-full object-cover"
       />
